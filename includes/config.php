e0e6e6eed20551dd06cb0871569b481b17b87dac
Drastik <info@pirateproxy.net>
2015-04-19 10:24:04 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
   # Add your custom code before the body (e.g. Google Analytics)
   $footerCode = <<<EOT
-<!-- <a href="https://proxybay.info">More Pirate Bay Proxies</a><br> -->
-<!-- <a href="https://proxybay.info/setup.html">Set up your own proxy</a><br> -->
+<!-- <a href="https://proxybay.co">More Pirate Bay Proxies</a><br> -->
+<!-- <a href="https://proxybay.co/setup.html">Set up your own proxy</a><br> -->
 EOT;
 ?>
