d173eac71a68dad8e443269b0114355607199377
Drastik <info@pirateproxy.net>
2016-01-17 11:07:33 -0500

    Update functions.php
    Added fix to display ads

--- includes/functions.php
+++ includes/functions.php
@@ -3,6 +3,7 @@ function remove_bloat($page, $footerCode = ''){
   $page = str_replace("/s/","/search.php", $page);
   $page = str_replace('//thepiratebay.se','', $page);
   $page = str_replace('href="','href="/?load=', $page);
+  $page = str_replace('src="/static/','src="/?load=/static/', $page);
   $page = str_replace('href="/?load=http','href="http', $page);
   $page = str_replace('href="/?load=/static','href="/static', $page);
   $page = str_replace('href="/?load=magnet','href="magnet', $page);
