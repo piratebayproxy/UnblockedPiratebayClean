fec62a92bff98112be4a7ab2919d433a0f17320d
Drastik <info@pirateproxy.net>
2016-05-26 11:24:42 -0400

    Update config.php
    Updated TPB url

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
   $mydomain = $_SERVER['HTTP_HOST'];
-  $domaintoproxy = "https://thepiratebay.se";
+  $domaintoproxy = "https://thepiratebay.org";
 
   # Add your custom code before the body (e.g. Google Analytics)
   $footerCode = <<<EOT
-<!-- <a href="https://proxybay.la">More Pirate Bay Proxies</a><br> -->
-<!-- <a href="https://proxybay.la/setup.html">Set up your own proxy</a><br> -->
+<!-- <a href="https://proxybay.tv">More Pirate Bay Proxies</a><br> -->
+<!-- <a href="https://proxybay.tv/setup.html">Set up your own proxy</a><br> -->
 EOT;
 ?>
