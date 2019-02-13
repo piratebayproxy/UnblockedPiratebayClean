69efe986c80c66e26b8cdec4b30a87be3d3a646c
PirateProxy <piratebayproxy@users.noreply.github.com>
2019-02-13 12:35:02 +0100

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
   # Add your custom code here and it will be inserted before the closing </body> tag (e.g. Google Analytics)
   $footerCode = <<<EOT
-<!-- <a href="https://proxybay.bet">More Pirate Bay Proxies</a><br> -->
-<!-- <a href="https://proxybay.bet/setup.html">Set up your own proxy</a><br> -->
+<!-- <a href="https://proxybay.cc">More Pirate Bay Proxies</a><br> -->
+<!-- <a href="https://proxybay.cc/setup.html">Set up your own proxy</a><br> -->
 EOT;
 ?>
