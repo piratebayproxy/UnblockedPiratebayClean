bd8ac622202618de72dc53117cdd28e34234b31a
PirateProxy <piratebayproxy@users.noreply.github.com>
2020-04-18 18:16:00 -0500

    Update search.php

--- search.php
+++ search.php
@@ -15,151 +15,20 @@
 <script>document.getElementById("jscrwarn").innerHTML='';</script>
 <b><font color="RED"><label id="jscrwarn2">Your adblock may block important javascript components, check that main.js is loaded or the webpage won't work.</label></font></b>
 <h1><label id="tlt"></label></h1>
-<div class="adblock" id="ad-top">
-<div class="ad728 align-center">
-</div>
-<div class="ad468 align-center">
-</div>
-<div class="ad234 align-center">
-</div>
-</div>
 <div class="browse">
-<section class="col-left ad120">
-</section>
-<section class="col-center">
-<ol id="torrents" class="view-single">
-<li class="list-header">
-<span class="list-item list-header item-type"><label onclick="sortlist(1);" title="Order by Category">Category</label></span>
-<span class="list-item list-header item-name"><label onclick="sortlist(2);" title="Order by Name">Name</label></span>
-<span class="list-item list-header item-uploaded"><label onclick="sortlist(3);" title="Order by Date Uploaded">Uploaded</label></span>
-<span class="list-item list-header item-icons">&nbsp;</span>
-<span class="list-item list-header item-size"><label onclick="sortlist(5);" title="Order by Size">Size</label></span>
-<span class="list-item list-header item-seed"><label onclick="sortlist(6);" title="Order by Seeders">SE</label></span>
-<span class="list-item list-header item-leech"><label onclick="sortlist(7);" title="Order by Leechers">LE</label></span>
-<span class="list-item list-header item-user"><label onclick="sortlist(8);" title="Order by ULed by">ULed by</label></span>
-</li>
-<script>if (typeof make_search !== "undefined" ) make_search();</script>
-</ol>
-</section>
+<script>print_search()</script>
 <section class="col-right ad120">
-</section>
-</div>
-<div class="adblock" id="ad-bottom">
-<div class="ad728 align-center">
-</div>
-<div class="ad468 align-center">
-</div>
-<div class="ad234 align-center">
-</div>
 </div>
 </main>
 <header class="row">
-<section class="col-left" id="logo"><a href="/"><img src="/static/images/tpbsmall_notext.jpg" alt="The Pirate Bay"></a></section>
-<section class="col-center">
-<nav>
-<a href="/" title="Search Torrents"><strong>Search&nbsp;Torrents</strong></a> |
-<a href="/browse.php" title="Browse Torrents">Browse&nbsp;Torrents</a> |
-<a href="/search.php?q=top100:recent" title="Recent Torrent">Recent&nbsp;Torrents</a> |
-<a href="/top.php" title="Top 100">Top&nbsp;100</a>
-</nav>
-<form action="/search.php">
+<script>print_header1()</script>
 <input type="search" title="Pirate Search" name="q" placeholder="Search here..." id="search">
-<script>
-      document.getElementById("search").value = getParameterByName('q');
-    </script>
-<input value="Pirate Search" type="submit">
-<select name="cat" id="cat">
- <option value="0">All</option>
-<optgroup label="Audio">
-<option value="101">Music</option>
-<option value="102">Audio books</option>
-<option value="103">Sound clips</option>
-<option value="104">FLAC</option>
-<option value="199">Other</option>
-</optgroup>
-<optgroup label="Video">
-<option value="201">Movies</option>
-<option value="202">Movies DVDR</option>
-<option value="203">Music videos</option>
-<option value="204">Movie clips</option>
-<option value="205">TV shows</option>
-<option value="206">Handheld</option>
-<option value="207">HD - Movies</option>
-<option value="208">HD - TV shows</option>
-<option value="209">3D</option>
-<option value="299">Other</option>
-</optgroup>
-<optgroup label="Applications">
-<option value="301">Windows</option>
-<option value="302">Mac</option>
-<option value="303">UNIX</option>
-<option value="304">Handheld</option>
-<option value="305">IOS (iPad/iPhone)</option>
-<option value="306">Android</option>
-<option value="399">Other OS</option>
-</optgroup>
-<optgroup label="Games">
-<option value="401">PC</option>
-<option value="402">Mac</option>
-<option value="403">PSx</option>
-<option value="404">XBOX360</option>
-<option value="405">Wii</option>
-<option value="406">Handheld</option>
-<option value="407">IOS (iPad/iPhone)</option>
-<option value="408">Android</option>
-<option value="499">Other</option>
-</optgroup>
-<optgroup label="Porn">
-<option value="501">Movies</option>
-<option value="502">Movies DVDR</option>
-<option value="503">Pictures</option>
-<option value="504">Games</option>
-<option value="505">HD - Movies</option>
-<option value="506">Movie clips</option>
-<option value="599">Other</option>
-</optgroup>
-<optgroup label="Other">
-<option value="601">E-books</option>
-<option value="602">Comics</option>
-<option value="603">Pictures</option>
-<option value="604">Covers</option>
-<option value="605">Physibles</option>
-<option value="699">Other</option>
-</optgroup>
-</select>
-</form>
-</section>
-<section class="col-right ad468">
-</section>
-<section class="col-right ad234">
-</section>
+<script>document.getElementById("search").value = getParameterByName('q')</script>
+<script>print_header2()</script>
 </header>
-<footer class="row flow-root flow-column align-center">
-<nav>
-<div>
-<a href="http://piratebayztemzmv.onion" title="tor address">TOR</a> |
-<a href="https://pirates-forum.org/" title="discussion forum" target="_blank">Forum</a> |
-<a href="https://www.azirevpn.com/landing" target='_NEW'><b>VPN</b></a> |
-<a href="https://tmp.ninja" target='_NEW'><b>FileHosting</b></a>
-</div>
-</nav>
-<p style="overflow-wrap: break-word;">
-<a href="https://bitcoin.org" target="_NEW">BTC</a>: <b>3EoJMDs79b3ztkYvj2E1D98eHnZyCSQKso</b>
-<br /><a href="https://bitcoin.org" target="_NEW">BTC (Bech32)</a>: <b>bc1q9x30z7rz52c97jwc2j79w76y7l3ny54nlvd4ew</b>
-<br /><a href="https://litecoin.org" target="_NEW">LTC</a>: <b>LS78aoGtfuGCZ777x3Hmr6tcoW3WaYynx9</b>
-<br /><a href="https://getmonero.org" target="_NEW">XMR</a>: <b>46E5ekYrZd5UCcmNuYEX24FRjWVMgZ1ob79cRViyfvLFZjfyMhPDvbuCe54FqLQvVCgRKP4UUMMW5fy3ZhVQhD1JLLufBtu</b>
-</p>
-</footer>
 <script>
-var scate = document.getElementById('cat');
-if (scate) {
-  var ct = getParameterByName('cat');
-  if ((Number(ct) > 99) && (Number(ct) < 700)) {
-    scate.value = ct;
-  } else {
-    scate.value = 0;
-  }
-}
+print_footer();
+mark_selected();
 </script>
 </body>
 </html>
